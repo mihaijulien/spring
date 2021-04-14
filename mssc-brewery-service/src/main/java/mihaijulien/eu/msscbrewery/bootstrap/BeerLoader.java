e23b7499a138884b916d0854b0aeb22332e50059
Julien Mihai <mihaijulien@gmail.com>
2021-04-14 21:35:44 +0300

    Add HikariCP configuration

--- mssc-brewery-service/src/main/java/mihaijulien/eu/msscbrewery/bootstrap/BeerLoader.java
+++ mssc-brewery-service/src/main/java/mihaijulien/eu/msscbrewery/bootstrap/BeerLoader.java
@@ -22,7 +22,9 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObjects();
+        if(beerRepository.count() == 0) {
+            loadBeerObjects();
+        }
     }
 
     private void loadBeerObjects() {
