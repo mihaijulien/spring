7fa03fe99193c15baac76e4b6f4dae6568de58bd
Julien Mihai <mihaijulien@gmail.com>
2021-04-30 12:23:01 +0300

    Setting route configuration example

---
+++ msssc-brewery-gateway/src/main/java/com/sfg/gateway/config/GoogleConfig.java
@@ -0,0 +1,22 @@
+package com.sfg.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleRouteConfig(RouteLocatorBuilder builder){
+        return builder.routes()
+                // http://localhost:9090/googlesearch -> redirect to https://google.com
+                .route(r -> r.path("/googlesearch")
+                        // rewrite the path so that '/googlesearch' is slashed from the final redirect uri
+                        .filters(f -> f.rewritePath("/googlesearch(<segment>/?.*)", "${segment}"))
+                .uri("https://google.com")
+                .id("google"))
+                .build();
+    }
+}
