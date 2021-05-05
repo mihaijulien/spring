ced4f6ba81a1f400f0c54fdd29b16a0200424c79
Julien Mihai <mihaijulien@gmail.com>
2021-05-05 16:09:03 +0300

    Add failover inventory service

---
+++ mssc-inventory-failover/src/main/java/mihaijulien/eu/SfgBreweryInventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package mihaijulien.eu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBreweryInventoryFailoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgBreweryInventoryFailoverApplication.class, args);
+    }
+
+}
